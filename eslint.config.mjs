e0ec4b7c7701e5d5b2d13a1bc416918fa98d99dc
LamberDev <lamberlemos@gmail.com>
2026-06-14 19:11:57 +0200

    chore(eslint): add ignores section

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,11 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  { ignores: ['dist/**', 'node_modules/**'] },
   ...tseslint.configs.recommended,
   {
     languageOptions: {
       parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
         ecmaFeatures: {
           experimentalDecorators: true,
         },
