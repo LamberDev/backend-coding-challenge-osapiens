995f5092f2bce33ced36b156c32683b43ae2aa31
LamberDev <lamberlemos@gmail.com>
2026-07-05 19:02:13 +0200

    feat(workflow_status_endpoint): use new modules

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import 'reflect-metadata';
 import express from 'express';
 import analysisRoutes from './routes/analysisRoutes';
+import workflowRoutes from './routes/workflowRoutes';
 import defaultRoute from './routes/defaultRoute';
 import { taskWorker } from './workers/taskWorker';
+import { errorMiddleware } from './http/errorMiddleware';
 import { AppDataSource } from './data-source'; // Import the DataSource instance
 
 const app = express();
 app.use(express.json());
 app.use('/analysis', analysisRoutes);
+app.use('/workflow', workflowRoutes);
 app.use('/', defaultRoute);
+app.use(errorMiddleware);
 
 AppDataSource.initialize()
   .then(() => {
